7ed2485e804f47320ece9924c5b98375e36c2c14
“Hdr0409” <m.haidarhaq09@gmail.com>
2025-12-02 13:12:02 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const cors = require("cors");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// API KEY
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+app.post("/chat", async (req, res) => {
+  try {
+    const { message } = req.body;
+
+    const model = genAI.getGenerativeModel({
+      model: "gemini-2.0-flash",
+    });
+
+    const result = await model.generateContent([{ text: message }]);
+
+    const text =
+      result.response.candidates?.[0]?.content?.parts?.[0]?.text ||
+      "AI tidak memberikan respons.";
+
+    res.json({ reply: text });
+  } catch (error) {
+    console.log(error);
+    res.json({ reply: "Error: " + error.message });
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
